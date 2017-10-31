33c5b301fb0b6a02c452c3e1711186f0ff09fee6
Alexander Ken Vega Garcia <vega.ken.developer@gmail.com>
2017-10-31 18:14:39 -0500

    started website 3

---
+++ website3/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Contact Us</title>
+  <link rel="stylesheet" href="https://bootswatch.com/cosmo/bootstrap.min.css">
+</head>
+<body>
+  <nav class="navbar navbar-default">
+    <div class="container">
+      <div class="navbar-header">
+        <a href="index.php" class="navbar-brand">My Website</a>
+      </div>
+    </div>
+
+    <form method="post" action="">
+      <div class="form-group">
+        <label for="">Name</label>
+        <input type="text" name="name" class="form-control">
+      </div>
+      <div class="form-group">
+        <label for="">Email</label>
+        <input type="text" name="email" class="form-control">
+      </div>
+      <div class="form-group">
+        <label for="">Message</label>
+        <textarea type="text" name="message" class="form-control"></textarea>
+      </div>
+      <br>
+      <button type="submit" name="submit" class="btn btn-primary">Submit</button>
+    </form>
+
+    https://www.youtube.com/watch?v=tJ5eUgOxITE&list=PLillGF-Rfqbap2IB6ZS4BBBcYPagAjpjn&index=15
+  </nav>
+</body>
+</html>
